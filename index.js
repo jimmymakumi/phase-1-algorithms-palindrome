edbdc50dbe8d02c18f19d72efc2f3cacd7513f07
jimmymakumi <146829497+jimmymakumi@users.noreply.github.com>
2023-10-22 11:02:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+for(let i=0; i<word; i++){
+  
 }
 
-/* 
-  Add your pseudocode here
-*/
+
+  // Write your algorithm here
+}
+function reverseString(word) {
+  // TODO: implement string reversing functionality
+  return word;
+}
+
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isPalindrome(word) {
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+
+  //Add your pseudocode here
+
 
 /*
   Add written explanation of your solution here
